650d90ccbb3d94777c9579965416932f8db25bc1
Your Name <you@example.com>
2023-07-23 18:45:48 +0100

    Index.ejs

---
+++ server.js
@@ -0,0 +1,22 @@
+let Express = require("express")
+let server = Express()
+let MySql = require("mysql")
+
+server.set("view engine", "ejs")
+
+let db = MySql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"root",
+    database: "nbs2"
+})
+db.connect()
+
+server.get("/", (req,res)=>{
+    res.render("index.ejs")
+    res.end()
+})
+
+server.listen(3000, ()=>{
+    console.log('Server is running...');
+})
