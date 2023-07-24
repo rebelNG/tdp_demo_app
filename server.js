b5e77ceeef6907742770c21f797902624e4218f6
Your Name <you@example.com>
2023-07-24 14:00:43 +0100

    Added new card function

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 let Express = require("express")
 let server = Express()
+let bodyParser = require('body-parser')
 // let MySql = require("mysql")
 
 server.set("view engine", "ejs")
+server.use(bodyParser.urlencoded({ extended: false }))
 
 // let db = MySql.createConnection({
 //     host: "localhost",
@@ -12,9 +14,26 @@ server.set("view engine", "ejs")
 // })
 // db.connect()
 
+let initialCard = []
 server.get("/", (req,res)=>{
-    res.render("index.ejs")
+    res.render("index.ejs",{initialCard})
     res.end()
+})
+
+server.post("/home", (req,res)=>{
+    let input0 = req.body.txt0
+    let input1 = req.body.txt1
+    let input2 = req.body.txt2
+    let input3 = req.body.txt3
+
+    let newcard = {
+        title: `${input0}`,
+        content: `${input1}\n${input2}\n${input3}`,
+        imageSrc:"#"
+    }
+    initialCard.push(newcard)
+
+    res.redirect("/");
 })
 
 server.get("/createpost", (req,res)=>{
